579fe3c5e66c434299ae95e158d02e6fa965a317
playful-fox <106951465+playful-fox@users.noreply.github.com>
2024-07-31 16:18:05 +0800

    initial

---
+++ src/goback.js
@@ -0,0 +1,12 @@
+const goback = () => {
+    return (
+        <div classname = "goback">
+            <h2>Air Quailty Map</h2>
+            <label>請輸入目的地：</label>
+            <input 
+            type = "text"
+            required
+            />
+        </div>
+    )
+}
